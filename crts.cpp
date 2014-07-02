1d4ff1677999eb17208724d2d70ae6bf6aa90589
User <user@user.(none)>
2014-07-01 22:50:55 -0400

    modified:   crts.cpp

--- crts.cpp
+++ crts.cpp
@@ -1361,7 +1361,7 @@ void * serveTCPDSAclient(void * _sc_ptr){
 					printf("Secondary Message %d\n", latestsecondary);
 				}
 			}*/
-			if(read_buffer.number > number){
+			if(read_buffer.number > number && (read_buffer.type == 'p' || read_buffer.type == 's')){
 				*m_ptr = read_buffer;
 				m_ptr->msgreceived = 1;
 				number = read_buffer.number;
