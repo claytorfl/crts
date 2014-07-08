494b96bb67fa54330322fcee098c93d626980d3f
User <user@user.(none)>
2014-07-07 20:42:17 -0400

    modified:   crts.cpp

--- crts.cpp
+++ crts.cpp
@@ -4081,7 +4081,7 @@ if(dsa && isController){
 	std::clock_t time = std::clock();
 	int loop = 1;
 	while(loop){
-		rssi = usrp->uhd::usrp::multi_usrp::read_rssi(5);
+		rssi = usrp->uhd::usrp::multi_usrp::read_rssi(3);
 		printf("%f\n", rssi);
 		if(msg.msgreceived == 1){
 			if(msg.type == 'P'){
