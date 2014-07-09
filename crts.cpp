d972a1061fa2e5da9147d02e3c3d868f4f5912a2
User <user@user.(none)>
2014-07-09 16:23:04 -0400

    modified:   crts.cpp

--- crts.cpp
+++ crts.cpp
@@ -2440,8 +2440,10 @@ int fftscan(struct CognitiveEngine ce){
 		//printf("8\n");
         fftwf_execute(p);
 		int x;
+        for (unsigned int i=0; i<out_buff.size();i++)
+             out_buff_norm[i]=pow(abs(out_buff[i]),2);
 		for(x=0; x<bw/chbw; x++){
-		printf("%d %g +i%g\n", x+1, out_buff[x]);
+		printf("%d %f\n", x+1, out_buff_norm[x]);
 		}
         //calculate avmfft from moving average function
         send_avmfft=Moving_Avg(out_buff,Moving_Avg_size);
