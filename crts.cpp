0cee7934a09321437584183c2b0adcc94c1474ad
User <user@user.(none)>
2014-07-14 15:50:49 -0400

    modified:   crts.cpp

--- crts.cpp
+++ crts.cpp
@@ -5450,25 +5450,25 @@ if(dsa && isController){
 	printf("\n%d Primary Frames\nAverage Primary Frame Feedback\n\n", pfb);
 	fprintf(dataFile, "\n%d Primary Frames\nAverage Primary Frame Feedback\n\n", pfb);
 	fprintf(dataFile, "%-10s %-10i %-10.2f %-10.2f %-8.2d %-12.2d %-12.2d %-20.2f %-19.2f\n", 
-		"puframes:", primaryfb.iteration,  primaryfb.evm, primaryfb.rssi, primaryfb.payloadByteErrors,
+		"puframes:  ", primaryfb.iteration,  primaryfb.evm, primaryfb.rssi, primaryfb.payloadByteErrors,
 		primaryfb.payloadBitErrors, 1, 1.0, 1.0);
 	feedbackStruct_print(&primaryfb);
 	printf("\n%d Primary Collision Frames\nAverage Primary Collision Frame Feedback\n\n", pcfb);
 	fprintf(dataFile, "\n%d Primary Collision Frames\nAverage Primary Collision Frame Feedback\n\n", pcfb);
 	fprintf(dataFile, "%-10s %-10i %-10.2f %-10.2f %-8.2d %-12.2d %-12.2d %-20.2f %-19.2f\n", 
-		"puCframes:", primarycollisionfb.iteration,  primarycollisionfb.evm, primarycollisionfb.rssi, primarycollisionfb.payloadByteErrors,
+		"puCframes:  ", primarycollisionfb.iteration,  primarycollisionfb.evm, primarycollisionfb.rssi, primarycollisionfb.payloadByteErrors,
 		primarycollisionfb.payloadBitErrors, 1, 1.0, 1.0);
 	feedbackStruct_print(&primarycollisionfb);
 	printf("\n%d Secondary Frames\nAverage Secondary Frame Feedback\n\n", sfb);
 	fprintf(dataFile, "\n%d Secondary Frames\nAverage Secondary Frame Feedback\n\n", sfb);
 	fprintf(dataFile, "%-10s %-10i %-10.2f %-10.2f %-8.2d %-12.2d %-12.2d %-20.2f %-19.2f\n", 
-		"suframes:", secondaryfb.iteration,  secondaryfb.evm, secondaryfb.rssi, secondaryfb.payloadByteErrors,
+		"suframes:  ", secondaryfb.iteration,  secondaryfb.evm, secondaryfb.rssi, secondaryfb.payloadByteErrors,
 		secondaryfb.payloadBitErrors, 1, 1.0, 1.0);
 	feedbackStruct_print(&secondaryfb);
 	printf("\n%d Secondary Collision Frames\nAverage Secondary Collision Frame Feedback\n\n", scfb);
 	fprintf(dataFile, "\n%d Secondary Collision Frames\nAverage Secondary Collision Frame Feedback\n\n", scfb);
 	fprintf(dataFile, "%-10s %-10i %-10.2f %-10.2f %-8.2d %-12.2d %-12.2d %-20.2f %-19.2f\n", 
-		"suCframes:", secondarycollisionfb.iteration,  secondarycollisionfb.evm, secondarycollisionfb.rssi, secondarycollisionfb.payloadByteErrors,
+		"suCframes:  ", secondarycollisionfb.iteration,  secondarycollisionfb.evm, secondarycollisionfb.rssi, secondarycollisionfb.payloadByteErrors,
 		secondarycollisionfb.payloadBitErrors, 1, 1.0, 1.0);
 	feedbackStruct_print(&secondarycollisionfb);
 	return 1;
